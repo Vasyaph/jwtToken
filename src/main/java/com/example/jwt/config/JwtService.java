ad0dbe253f6d40a7dd918d2b0633df0d7c3d0ca6
Vladyslav Yehorov <Vasyaph@gmail.com>
2023-06-19 18:23:17 +0300

    fixing bugs

--- src/main/java/com/example/jwt/config/JwtService.java
+++ src/main/java/com/example/jwt/config/JwtService.java
@@ -40,8 +40,12 @@ public class JwtService {
     }
     public boolean isTokenValid(String token,UserDetails userDetails){
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername())) &&isTokenExpired(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
 
+    }
+    public String generateToken(Map<String,Object> extraClaims,UserDetails userDetails){
+
+        return buildToken(extraClaims, userDetails, jwtExpiration);
     }
 
     private boolean isTokenExpired(String token) {
@@ -52,10 +56,7 @@ public class JwtService {
         return extractClaim(token,Claims::getExpiration);
     }
 
-    public String generateToken(Map<String,Object> extraClaims,UserDetails userDetails){
 
-        return buildToken(extraClaims, userDetails, jwtExpiration);
-    }
     private String buildToken(
             Map<String, Object> extraClaims,
             UserDetails userDetails,
